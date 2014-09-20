eb619ea05d5d1ecf22831ea1c0ef897e43bd2af0
Hiral Patel <ahirapatel@gmail.com>
2014-09-20 12:56:51 -0400

    Added docstrings

--- progressbar.py
+++ progressbar.py
@@ -27,10 +27,13 @@ class ProgressBar(object):
             self._fillchar = fillchar
 
     # TODO: Get the size of the terminal (Get it everytime here or just once in __init__ ?)
-    # If msg=None, then use previous message, if no previous message, do not use second row.
-    # If msg=<some string> print it and store it as the previously used string.
-    # If msg=<some string>, then upon next call msg=None, the old string will be printed.
     def update_progress(self, msg = None):
+        """
+        Update the progress bar status and print it.
+        If msg=None, then use previous message, if no previous message, do not use second row.
+        If msg=<some string> print it and store it as the previously used string.
+        If msg=<some string>, then upon next call msg=None, the old string will be printed.
+        """
         if self._curr > self._steps:
             raise Exception("Progress bar already completed")
 
@@ -51,26 +54,44 @@ class ProgressBar(object):
         self._curr += 1
 
     def move_up_as_needed(self):
+        """
+        Move up in the term once or twice, depending on if you have a message.
+        """
         if self._curr != 0:
             self.move_up_one_line()
             if self._msg != None:
                 self.move_up_one_line()
 
     def get_terminal_width(self):
+        """
+        Get the width of the terminal.
+        """
         import os
         rows, columns = os.popen('stty size', 'r').read().split()
         return int(columns)
 
     def move_to_start_of_line(self):
+        """
+        Moves to the start of the current line.
+        """
         print "\r",
 
     def move_up_one_line(self):
+        """
+        Moves up one line in the terminal.
+        """
         print "\033[1A",
 
     def clear_curr_line(self):
+        """
+        Clears the current line by filling with spaces then moves to the start.
+        """
         print ' '.ljust(self.get_terminal_width()-1), '\r',
 
     def get_progress_bar(self):
+        """
+        Outputs the progress bar string.
+        """
         reserved_char_count = 2     # For start and ending character of the progress bar.
         progress_chars_max = self.get_terminal_width() - reserved_char_count
         progress_chars_max /= 4     # Arbitrarily make it 1/4th the number of chars that can fit onscreen.
